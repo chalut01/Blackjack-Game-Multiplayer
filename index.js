4e93b4aabb14726102afd98a8452431abf3aac23
chalut <chalut@techberry.co.th>
2021-08-07 18:49:42 +0700

    3100

--- index.js
+++ index.js
@@ -5,31 +5,17 @@ const { client } = require("websocket");
 const { join } = require("path");
 const app = express();
 const server = require("http").createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3100;
 const WebSocket = require("ws")
 
 const wss = new WebSocket.Server({ server:server })
-
-
-
 
 // Serve all the static files, (ex. index.html app.js style.css)
 app.use(express.static("public/"));
 // Before 8081
 server.listen(PORT, () =>
-  console.log(`Listening on ${process.env.PORT} or 3000`)
+  console.log(`Listening on ${process.env.PORT} or 3100`)
 );
-
-
-
-
-
-
-
-
-
-
-
 
 
 // hashmap clients
@@ -70,7 +56,7 @@ wss.on("connection", (ws) => { // wsServer || wss AND request || connection
       const playerSlotHTML = result.playerSlotHTML;
       const offline = result.offline;
       const roomId = partyId();
-      const gameId = `http://localhost:3000/` + roomId;
+      const gameId = `http://13.213.149.148:3100/` + roomId;
 
       app.get("/" + roomId, (req, res) => {
         res.sendFile(__dirname + "/public/index.html");
